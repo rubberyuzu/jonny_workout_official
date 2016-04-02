4d3968d2580061e4b08e90a8c5921ac4c15661d6
Yuzu Saijo <yuzu0125@gmail.com>
2016-04-02 15:13:12 +0900

    WIP

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: 35.6895, lng: 139.6917}
+  });
+  var georssLayer = new google.maps.KmlLayer({
+    url: 'http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss'
+  });
+  georssLayer.setMap(map);
+}
+
 $(document).ready(function(){
 
   // TODO: change the easing here to whatever more smooth. :) 
